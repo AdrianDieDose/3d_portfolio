f1e3512a4a1945c7d23285463a6aada8f558b220
AdrianDieDose <AdrianDuda2019@gmail.com>
2021-06-26 15:20:05 +0200

    Trying to make mesh plane, made rect points

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ import * as THREE from 'three';
 
 
 
+
+var Theme = {_darkred: 0x000000}
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -18,20 +20,13 @@ renderer.setPixelRatio( window.devicePixelRatio );
 renderer.setSize( window.innerWidth, window.innerHeight );
 camera.position.setZ( 60 );
 camera.position.setY(10);
+
 camera.rotation.x = 0.2;
+
 
 
 renderer.render( scene, camera );
 
-const ahegao = new THREE.TextureLoader().load("gay.jpg");
-
-const plane = new THREE.Mesh(
-  new THREE.BoxGeometry(10,10,10),
-  new THREE.MeshStandardMaterial({
-    map: ahegao,
-  })
-)
-scene.add(plane);
 
 const pointLight = new THREE.PointLight(0xffffff);
 pointLight.position.set(20,20,20);
@@ -41,95 +36,96 @@ const ambientLight = new THREE.AmbientLight(0xffffff);
 scene.add(pointLight,ambientLight);
 
 const gridHelper = new THREE.GridHelper(200, 50);
-//scene.add(gridHelper);
+scene.add(gridHelper);
+
+const planeHeight = 20;
+const planeWidth = 20;
+const pointsAmount = 10;
+const geomentry = new THREE.PlaneGeometry(planeHeight, planeWidth);
+    const material = new THREE.MeshStandardMaterial({wireframe: true});
+    const sphereOutOfPoints = new THREE.Mesh(geomentry, material);
+    sphereOutOfPoints.name = "pointsSphere";
+    scene.add(sphereOutOfPoints);
+    
+const Point = {
+  pointNumb: 0,
+  x: -planeHeight/2,
+  y: -planeWidth/2,
+  z: 0,
+  makePoint: function() {
+    const geomentry = new THREE.SphereGeometry(0.2, 24, 24);
+    const material = new THREE.MeshStandardMaterial({color: 0x19297C,});
+    const point = new THREE.Mesh(geomentry, material);
+    //this.xyz = this.xyz[this.xyz[0]+1, this.xyz[1]+0.5, this.xyz[2]+0.1];
+    point.position.set(Point.x,Point.y,Point.z);
+    sphereOutOfPoints.add(point);
+    point.name = point;
+    Point.x+=planeHeight /pointsAmount;
+    Point.z+=0;
+    Point.y+=planeHeight /pointsAmount;
+  }
+}
 
 
-
-function addRainDrop() {
-
-  
+Array(pointsAmount).fill().forEach(Point.makePoint);
+console.log(Point.x,Point.y,Point.z);
+console.log(scene.children);
+/*
+let pointNumb = 0;
+function addMesh() {
   const geomentry = new THREE.SphereGeometry(0.2, 24, 24);
   const material = new THREE.MeshStandardMaterial( {
     color: 0x19297C,
-    opacity: 0.2,
   });
-  const drop = new THREE.Mesh(geomentry, material);
+  const point = new THREE.Mesh(geomentry, material);
 
 
 
-  const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(200));
-  drop.position.set(x,y,z);
-  drop.name = drop;
-  scene.add(drop);
+  const [x] = pointNumb + 1;
+  const [z] = pointNumb + 0.5;
+  const [y] = pointNumb + 0.1;
+  point.position.set(x,y,z);
+  point.name = point;
+  scene.add(point);
 }
 
-Array(400).fill().forEach(addRainDrop);
+
+Array(400).fill().forEach(addMesh);
+*/
 
 
 
-const forestTexture = new THREE.TextureLoader().load("land2.png");
-scene.background = forestTexture;
 
+scene.background = new THREE.Color(Theme._darkred);
+
+/*
 
 function moveCam(){
 
 const t = document.body.getBoundingClientRect().top;
 const dir = document.body.scrollTop;
 
-  // Very inefficiant way of making rain... pls stop
 
-  let velocity;
-  window.onscroll = function(e) {
-    velocity = 3;
-    // print "false" if direction is down and "true" if up
-
-
-    
-    if(this.oldScroll > this.scrollY) {
-      
-      for(let i = 4;i< scene.children.length -4;i++){
-
-        scene.children[i].position.y += velocity;
-
-        if(scene.children[i].position.y >= 100){
-          scene.children[i].position.y = THREE.MathUtils.randFloatSpread(100);
-          
-        }
-      }
-    } else {
-      for(let i = 3;i< scene.children.length -4;i++){
-        
-        scene.children[i].position.y -= velocity;
-
-        if(scene.children[i].position.y <= 0 ){
-          scene.children[i].position.y = THREE.MathUtils.randFloatSpread(200);
-          
-        }
-      }
-    }
-    this.oldScroll = this.scrollY;
-    
+  
 
 }
-}
-document.body.onscroll = moveCam
+*/
+
 
 
 function animate() {
   requestAnimationFrame( animate );
 
+  
 
-plane.rotateY(0.010);
-plane.rotateZ(0.05);
-plane.rotateX(0.01);
   
 
 
-
+  
  
   
   renderer.render( scene, camera );
-
+  
 }
 
 
