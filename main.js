33e66213cbd4dcf05681261f31d5a6bf83c29f7b
Adrian_DieDose <45864030+AdrianDieDose@users.noreply.github.com>
2021-08-25 17:20:43 +0200

    Not ratio on load bug
    trying to fix

--- main.js
+++ main.js
@@ -19,15 +19,12 @@ const renderer = new THREE.WebGL1Renderer({
 //renderer.setPixelRatio(window.devicePixelRatio);
 
 // SETS ALL FRONT PAGE SIZES
-
-
-
-  const tvImage = document.getElementsByClassName("tv-overlay")[0];
-  renderer.setSize(tvImage.clientWidth * 0.8, tvImage.clientHeight * 0.63);
-  document.getElementsByClassName("tv-text")[0].style.height =
-    tvImage.clientHeight * 0.63 + "px";
-  document.getElementsByClassName("tv-text")[0].style.width =
-    tvImage.clientWidth * 0.7 + "px";
+const tvImage = document.getElementsByClassName("tv-overlay")[0];
+renderer.setSize(tvImage.clientWidth * 0.8, tvImage.clientHeight * 0.63);
+document.getElementsByClassName("tv-text")[0].style.height =
+  tvImage.clientHeight * 0.63 + "px";
+document.getElementsByClassName("tv-text")[0].style.width =
+  tvImage.clientWidth * 0.7 + "px";
 
 
 
