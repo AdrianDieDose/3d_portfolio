572cce2240120e7a8f13edcf1af561c0f8a45565
AdrianDieDose <AdrianDuda2019@gmail.com>
2021-08-26 02:39:15 +0200

    Changed renderer from WebGL1 to WebGL to fix bug

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ const camera = new THREE.PerspectiveCamera(
   1000
 );
 
-const renderer = new THREE.WebGL1Renderer({
+const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector("#bg"),
 });
 
